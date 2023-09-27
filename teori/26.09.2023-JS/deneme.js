78e11165481703e45bcaf7227a61842ba003fc98
aliguness <aligunescomu@gmail.com>
2023-09-27 17:16:31 +0300

    callback

--- teori/26.09.2023-JS/deneme.js
+++
@@ -1,9 +0,0 @@
-const ages=[32,31,25,25,29,28,28,28,26,27,31,31,27,27];
-
-function getAges(ages, ...rest){  // ... spread deniyor.
-    for ( const age of [...ages, ...rest]){
-        console.log(age)
-    }
-
-}
-getAges(ages, 24,35,26,27,18)
