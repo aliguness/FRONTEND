bb353c720eb1b09c7073ef1dc96b0b164bfd19ba
aliguness <aligunescomu@gmail.com>
2023-09-27 17:17:34 +0300

    callback

--- teori/JS/14functions.js
+++ teori/JS/14functions.js
@@ -1,34 +1,36 @@
 const orhun = {
-    name: "Orhun",
-    surname: "Bayındır",
-    date: 1995,
-    town: "Ankara",
-    getInfo: function () {
-      console.log(`${this.name}'un doğum yılı ${this.date}`);
-    },
-  };
-  
-  function getOrhunsInfo() {
-    for (const key in orhun) {
-      const value = orhun[key];
-      console.log(`${key}: ${value}`);
-    }
+  name: "Orhun",
+  surname: "Bayındır",
+  date: 1995,
+  town: "Ankara",
+  // method
+  getInfo: function () {
+    console.log(`${this.name}'un doğum yılı ${this.date}`);
+  },
+};
+
+function getOrhunsInfo() {
+  for (const key in orhun) {
+    const value = orhun[key];
+    console.log(`${key}: ${value}`);
   }
-  
-  const getOrhunsLife = function () {
-    printDescription();
-    getOrhunsInfo();
-    orhun.getInfo();
-  };
-  
-  // * Arrow Function
-  const printDescription = () => {
-    console.log("Orhun'un özellikleri:");
-  };
-  
-  getOrhunsLife();
-  
-  // Hungarian Notation // ! bunu tercih etmiyoruz
-  const numberGetLength = () => {
-    return 5;
-  };
+}
+
+const getOrhunsLife = function () {
+  printDescription();
+  getOrhunsInfo();
+  orhun.getInfo();
+};
+
+// * Arrow Function ES6
+const printDescription = () => {
+  console.log("Orhun'un özellikleri:");
+};
+
+getOrhunsLife();
+
+// * Hungarian Notation
+// ! bunu tercih etmiyoruz
+const numberGetLength = () => {
+  return 5;
+};
